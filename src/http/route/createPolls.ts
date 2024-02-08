d5dc49f20195717a2d2dc9a85149d24000a8f8ee
Gabriel Reis <gabsrs@gitbasic.com>
2024-02-08 01:20:58 -0300

    feat: add options in create poll route

--- src/http/route/createPolls.ts
+++ src/http/route/createPolls.ts
@@ -6,15 +6,32 @@ import { FastifyInstance } from "fastify";
 export async function createPoll(app: FastifyInstance) {
     app.post("/polls", async (request, reply) => {
         const createPollsBody = z.object({
-            title: z.string()
+            title: z.string(),
+            options: z.array(z.string()),
         })
     
-        const { title } = createPollsBody.parse(request.body)
+        const { title, options } = createPollsBody.parse(request.body)
     
         const poll = await prisma.poll.create({
             data: {
                 title,
+                options: {
+                    createMany: {
+                        data: options.map(option => {
+                            return { title: option } 
+                        }),
+                    }
+                },
             }
+        })
+
+        await prisma.pollOption.createMany({
+            data: options.map(option => {
+                return {
+                    title: option,
+                    pollId: poll.id
+                }
+            })
         })
     
         return reply.status(201).send({ pollId: poll.id })
