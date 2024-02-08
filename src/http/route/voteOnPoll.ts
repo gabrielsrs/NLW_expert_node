51dd27cecaacce4ec9e7a21198fff311b0f2c06f
Gabriel Reis <gabsrs@gitbasic.com>
2024-02-08 03:17:22 -0300

    feat: setup user vote in a poll Validation just a vonte on poll Change vote from a same poll Save current vote

--- src/http/route/voteOnPoll.ts
+++ src/http/route/voteOnPoll.ts
@@ -18,6 +18,28 @@ export async function voteOnPoll(app: FastifyInstance) {
 
         let { sessionId } = request.cookies
 
+        if(sessionId) {
+            const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+                where: {
+                    sessionId_pollId: {
+                        sessionId,
+                        pollId
+                    },
+                },
+            })
+
+            if(userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+                await prisma.vote.delete({
+                    where: {
+                        id: userPreviousVoteOnPoll.id,
+                    },
+                })
+
+            } else if (userPreviousVoteOnPoll) {
+                return reply.status(400).send("You already vote on this poll.")
+            }
+        }
+
         if(!sessionId) {
             sessionId = randomUUID()
             reply.setCookie("sessionId", sessionId, {
@@ -28,6 +50,14 @@ export async function voteOnPoll(app: FastifyInstance) {
             })
         }
 
-        return reply.status(201).send({ sessionId })
+        await prisma.vote.create({
+            data: {
+                sessionId,
+                pollId,
+                pollOptionId,
+            }
+        })
+
+        return reply.status(201).send()
     })
 }
