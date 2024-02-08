19302d480d4e1c435b2eeccbbfc09c52385743e0
Gabriel Reis <gabsrs@gitbasic.com>
2024-02-08 01:34:48 -0300

    feat: create get poll route Create a get from poll route, where ruturn the poll informations either its options

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
 import { createPoll } from "../http/route/createPolls"
+import { getPoll } from "../http/route/getPoll"
 
 const app = fastify()
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({port: 3000}).then(() => {
     console.log("Server is running!!")
