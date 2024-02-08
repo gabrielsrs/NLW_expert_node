29347666e3bfa829eac0e45cb3d53adc2f0e3da2
Gabriel Reis <gabsrs@gitbasic.com>
2024-02-08 01:58:57 -0300

    feat: create base from vote route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,13 @@
 import fastify from "fastify";
 import { createPoll } from "../http/route/createPolls"
 import { getPoll } from "../http/route/getPoll"
+import { voteOnPoll } from "../http/route/voteOnPoll"
 
 const app = fastify()
 
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({port: 3000}).then(() => {
     console.log("Server is running!!")
